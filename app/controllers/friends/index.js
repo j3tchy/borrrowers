a218836ef649a0ef09189230f69aaf2c24e1e7be
Jermaine Gyekye <jgyekye@brightsource.co.uk>
2015-12-02 22:48:01 +0000

    sorting

--- app/controllers/friends/index.js
+++ app/controllers/friends/index.js
@@ -1,16 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	queryParams: ['sortBy','sortAscending'],
 	sortAscending: true,
 	sortBy: 'first_name',
-
-	possibleStates: ['borrowed','returned'],
-	contentDidChange: Ember.observer('model.[]', function(){
-		console.log('Called when we add or remove an article');
-	}),
-	stateDidChange: Ember.observer('model.@each.state', function(){
-		console.log('Called when the state property change for any of the articles');
-	}),
 
 	actions: {
 		setSortBy: function(field_name){
